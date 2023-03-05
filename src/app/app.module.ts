ae6b05e7c1f33a7ddeff539a8d4474b0ddea54f1
Patrick Kabuga <kabugap02@gmail.com>
2023-03-05 13:54:29 +0300

    [feat]: Add End Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { GameComponent } from './components/game/game.component';
 import { HighScoresComponent } from './components/high-scores/high-scores.component';
+import { EndComponent } from './components/end/end.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     GameComponent,
-    HighScoresComponent
+    HighScoresComponent,
+    EndComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/end/end.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-end',
+  templateUrl: './end.component.html',
+  styleUrls: ['./end.component.css']
+})
+export class EndComponent {
+
+}
