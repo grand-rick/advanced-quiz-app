535776353828e28a1193f1aa5753f595a52e2699
Patrick Kabuga <kabugap02@gmail.com>
2023-03-05 20:48:32 +0300

    [chore]: Initialize Question model

--- src/app/models/Question.ts
+++ src/app/models/Question.ts
@@ -1,7 +1,15 @@
-export type Question = {
+export class Question {
     category: string;
     difficulty: string;
     question: string;
     correct_answer: string;
     incorrect_answers: string[];
+
+    constructor () {
+        this.category = '';
+        this.difficulty = '';
+        this.question = '';
+        this.correct_answer = '';
+        this.incorrect_answers = ['', '', '', ''];
+    }
 }
