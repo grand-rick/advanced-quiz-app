e60e085cedee8fcc3c99cfbfd467affeffcd906e
Patrick Kabuga <kabugap02@gmail.com>
2023-03-05 13:47:44 +0300

    [chore]: Add routing for HighScores Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { GameComponent } from './components/game/game.component';
+import { HighScoresComponent } from './components/high-scores/high-scores.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'game', component: GameComponent }
+  { path: 'high-scores', component: HighScoresComponent }
 ];
 
 @NgModule({
