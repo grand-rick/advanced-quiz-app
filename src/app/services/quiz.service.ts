a42e3996d91a74693cc00b9a4a524af77f706cd5
Patrick Kabuga <kabugap02@gmail.com>
2023-03-05 16:29:21 +0300

    [chore]: Add getQuestions method

--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { Question } from '../models/Question';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
+  // questions: Question[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getQuestions(): Observable<Question[]> {
+    return this.http.get<Question[]>('assets/data.json');
+  }
 }
