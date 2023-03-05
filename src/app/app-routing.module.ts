b78f65d279324342c8251402d2c59200c57cb206
Patrick Kabuga <kabugap02@gmail.com>
2023-03-05 13:45:35 +0300

    [chore]: Add routing for Game Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { GameComponent } from './components/game/game.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'game', component: GameComponent }
 ];
 
 @NgModule({
