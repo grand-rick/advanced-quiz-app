fd9b2e04b96e05f852e364a579a55c5f685f2d10
Patrick Kabuga <kabugap02@gmail.com>
2023-03-21 13:57:37 +0300

    [chore]: Correct typo

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -47,7 +47,7 @@ export class GameComponent implements OnInit {
     for (let i = 0, n = unFormattedChoices.length; i < n; i++) {
       this.choices[i] = {
         choice: unFormattedChoices[i],
-        prefix: String.fromCharCode(this.prefixAsciiValue);,
+        prefix: String.fromCharCode(this.prefixAsciiValue),
         data_number:  (i+1).toString()
       }
       this.prefixAsciiValue++;
