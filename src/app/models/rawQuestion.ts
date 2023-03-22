cee2d8c969cfb0adbea49762d345a431ffc69a11
Patrick Kabuga <kabugap02@gmail.com>
2023-03-22 22:05:04 +0300

    [feat]: Add new model

---
+++ src/app/models/rawQuestion.ts
@@ -0,0 +1,15 @@
+export class rawQuestion {
+    category: string;
+    difficulty: string;
+    question: string;
+    correct_answer: string;
+    incorrect_answers: string[]
+
+    constructor () {
+        this.category = '';
+        this.difficulty = '';
+        this.question = '';
+        this.correct_answer = '';
+        this.incorrect_answers = [];
+    }
+}
