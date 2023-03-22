28d5474fd6232ecb7b8907926c047013fe64e3ee
Patrick Kabuga <kabugap02@gmail.com>
2023-03-23 02:45:06 +0300

    [feat]: Add starter logic

--- src/app/components/end/end.component.ts
+++ src/app/components/end/end.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-end',
   templateUrl: './end.component.html',
   styleUrls: ['./end.component.css']
 })
-export class EndComponent {
+export class EndComponent implements OnInit {
+  username: string = '';
 
+  constructor () {}
+
+  ngOnInit(): void {}
+
+  saveHighScore(event: MouseEvent) : {
+
+  }
 }
