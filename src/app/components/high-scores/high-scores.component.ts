f547093ebe3b67f0e5af8194bbe8cf4630e99f9d
Patrick Kabuga <kabugap02@gmail.com>
2023-03-23 17:47:01 +0300

    [feat]: Add Highscores logic

--- src/app/components/high-scores/high-scores.component.ts
+++ src/app/components/high-scores/high-scores.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizService } from 'src/app/services/quiz.service';
+import { Player } from 'src/app/models/Player';
 
 @Component({
   selector: 'app-high-scores',
   templateUrl: './high-scores.component.html',
   styleUrls: ['./high-scores.component.css']
 })
-export class HighScoresComponent {
+export class HighScoresComponent implements OnInit {
+  players: Player[] = this.quizService.getPlayers();
 
+  constructor (private quizService: QuizService) {}
+
+  ngOnInit(): void {}
 }
