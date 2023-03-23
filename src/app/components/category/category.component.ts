07cc6e285fcc00c97ec007a9f4668809af56ff7b
Patrick Kabuga <kabugap02@gmail.com>
2023-03-23 23:23:28 +0300

    [feat]: Add category as accordion

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface AccordionItem {
+  title: string;
+  content: string;
+  isOpen: boolean;
+}
+
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent {
+  public items: AccordionItem[] = [
+    { title: 'Item 1', content: 'This is the content for item 1.', isOpen: false },
+    { title: 'Item 2', content: 'This is the content for item 2.', isOpen: false },
+    { title: 'Item 3', content: 'This is the content for item 3.', isOpen: false },
+  ];
 
+  public toggleAccordion(item: AccordionItem): void {
+    item.isOpen = !item.isOpen;
+  }
 }
